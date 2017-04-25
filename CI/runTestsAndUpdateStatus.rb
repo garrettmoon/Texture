e94502ea9f7d2ce2759c1e4e53a046e0b44e5b3d
Garrett Moon <garrett@pinterest.com>
2017-04-25 12:49:20 -0700

    adding build phase

--- CI/runTestsAndUpdateStatus.rb
+++ CI/runTestsAndUpdateStatus.rb
@@ -1,13 +1,24 @@
 require "octokit"
-require "pp"
+# require "pp"
 
 client = Octokit::Client.new(access_token: ENV["CUSTOM_BUILD_CI_API_TOKEN"])
-pp client.user
-repository_uri = URI(ENV["BUILDKITE_REPO"])
-repository = repository_uri.path[1..-5]
-pp repository
-client.create_status(repository, ENV["BUILDKITE_COMMIT"], "pending", { 
-  :context => "CI/Pinterest", 
-  :target_url => ENV["BUILDKITE_BUILD_URL"], 
-  :description => "Building…"
-})
+
+def update_status(state, description)
+  repository_uri = URI(ENV["BUILDKITE_REPO"])
+  repository = repository_uri.path[1..-5]
+  client.create_status(repository, ENV["BUILDKITE_COMMIT"], state, { 
+    :context => "CI/Pinterest", 
+    :target_url => ENV["BUILDKITE_BUILD_URL"], 
+    :description => description
+  })
+end
+
+update_status("pending", "Building…")
+
+%x(./build_tmp.sh)
+
+if $CHILD_STATUS == 0
+  update_status("success", "All tests ran successfully.")
+else
+  update_status("failure", "Tests failed.")
+end
