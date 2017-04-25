5eabf53e45c609324b842422d42546438b505580
Garrett Moon <garrett@pinterest.com>
2017-04-25 14:01:52 -0700

    adding build phase

--- CI/runTestsAndUpdateStatus.rb
+++ CI/runTestsAndUpdateStatus.rb
@@ -2,10 +2,10 @@ require "octokit"
 require "pp"
 
 client = Octokit::Client.new(access_token: ENV["CUSTOM_BUILD_CI_API_TOKEN"])
+repository_uri = URI(ENV["BUILDKITE_REPO"])
+repository = repository_uri.path[1..-5]
 
-def update_status(client, state, description)
-  repository_uri = URI(ENV["BUILDKITE_REPO"])
-  repository = repository_uri.path[1..-5]
+def update_status(client, repo, state, description)
   client.create_status(repository, ENV["BUILDKITE_COMMIT"], state, { 
     :context => "CI/Pinterest", 
     :target_url => ENV["BUILDKITE_BUILD_URL"], 
@@ -13,7 +13,7 @@ def update_status(client, state, description)
   })
 end
 
-update_status(client, "pending", "Building…")
+update_status(client, repository, "pending", "Building…")
 
 log_path = "log.txt"
 
@@ -23,7 +23,7 @@ File.write(log_path, log)
 
 puts "Build status: '#{$?}'"
 if $? == 0
-  update_status(client, "success", "All tests ran successfully.")
+  update_status(client, repository, "success", "All tests ran successfully.")
 else
   if File.exist?(log_path)
     gist = client.create_gist({
@@ -36,9 +36,9 @@ else
       }
     })
     
-    pp gist
+    client.add_comment(repository, ENV["BUILDKITE_PULL_REQUEST"], "Build log:\n#{gist["url"]}")
   end
   
-  update_status(client, "failure", "Tests failed.")
+  update_status(client, repository, "failure", "Tests failed.")
   abort("Tests failed")
 end
