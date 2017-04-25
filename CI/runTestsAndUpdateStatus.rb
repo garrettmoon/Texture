eb5e05cf8742b33bbfe363bde8bd8189310c9a82
Garrett Moon <garrett@pinterest.com>
2017-04-25 11:19:20 -0700

    moving to ruby

--- CI/runTestsAndUpdateStatus.rb
+++ CI/runTestsAndUpdateStatus.rb
@@ -3,7 +3,7 @@ require "pp"
 
 client = Octokit::Client.new(access_token: ENV["CUSTOM_BUILD_CI_API_TOKEN"])
 repository_uri = URI(ENV["BUILDKITE_REPO"])
-pp repository_uri.path
+pp repository_uri.path[1..-1]
 client.create_status(ENV["BUILDKITE_REPO"], ENV["BUILDKITE_COMMIT"], "pending", { 
   :context => "CI/Pinterest", 
   :target_url => ENV["BUILDKITE_BUILD_URL"], 
